104dfd42e17c08f434adf3555d1531a752caf0e6
Carlos Kruger <carloskr@yahoo.com>
2022-11-08 18:52:56 -0500

    2.6

--- 2.6/employeeManagerTest.test.ts
+++ 2.6/employeeManagerTest.test.ts
@@ -9,28 +9,47 @@ import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
       driver: WebDriver;
       url: string = "https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html";
         //FILL OUT LOCATORS CONSTRUCTOR AND METHODS IN ORDER TO PASS THE TEST
+        addEmployee: By = By.name('addEmployee')
+        headerLogo: By = By.className('titleText')
+        nameInput: By = By.name('nameEntry')
+        phoneInput: By = By.name('phoneEntry')
+        titleInput: By = By.name('titleEntry')
+        constructor(driver: WebDriver) {
+            this.driver = driver
+        }
+        async navigate() {
+            await this.driver.get(this.url)
+            await this.driver.wait(until.elementLocated(this.headerLogo))
+            await this.driver.wait(until.elementIsVisible(await driver.findElement(this.addEmployee)))
+        }
+        async sendKeys(elementBy: By, keys) {
+            await this.driver.wait(until.elementLocated(elementBy));
+            return driver.findElement(elementBy).sendKeys(keys);
+        
+        }
   }
+  const emPage = new employeePage(driver); 
+  
 
-  describe("Employee Manger Test", () => {
+  describe("Employee Manager Test", () => {
       beforeEach(async () => {
-          await employeePage.navigate();
+          await emPage.navigate();
       })
       afterAll(async () => {
           await driver.quit()
       })
       test("adding an employee", async () => {
-          await driver.wait(until.elementLocated(emPage.header))
-          await driver.findElement(emPage.addEmployee).click()
-          await driver.findElement(emPage.newEmployee).click()
+          await driver.wait(until.elementLocated(emPage.headerLogo))
+          await driver.findElement(emPage.addEmployee)
           await driver.findElement(emPage.nameInput).click()
           await driver.findElement(emPage.nameInput).clear()
-          await driver.findElement(emPage.nameInput).sendKeys("Change this")
+          await driver.findElement(emPage.nameInput).sendKeys("Carlos")
           await driver.findElement(emPage.phoneInput).click()
           await driver.findElement(emPage.phoneInput).clear()
-          await driver.findElement(emPage.phoneInput).sendKeys("Change this")
+          await driver.findElement(emPage.phoneInput).sendKeys("99999")
           await driver.findElement(emPage.titleInput).click()
           await driver.findElement(emPage.titleInput).clear()
-          await driver.findElement(emPage.titleInput).sendKeys("Change this")
+          await driver.findElement(emPage.titleInput).sendKeys("Vice President")
   })
-
+  })
   /* this is a commment */
--- 2.6/testWithSelectors.test.ts
+++ 2.6/testWithSelectors.test.ts
@@ -12,22 +12,22 @@ describe("Filling in the blanks", () => {
         await driver.quit();
     })
 
-    const hdrInput: By = By. //fill in the blank
-    const mkeInput: By = By. //fill in the blank
-    const oaiInput: By = By. //fill in the blank
-    const nameInput: By = By. //fill in the blank
-    const clrBtn: By = By. //fill in blank 
-    const submitBtn: By = By. //fill in blank
-    const errorMsg: By = By. // fill in blank 
+    const hdrInput: By = By.name("hdrInput") //fill in the blank
+    const mkeInput: By = By.name("mkeInput") //fill in the blank
+    const oaiInput: By = By.name("oriInput") //fill in the blank
+    const nameInput: By = By.name("namInput") //fill in the blank
+    const clrBtn: By = By.id("clearBtn") //fill in blank 
+    const submitBtn: By = By.id("saveBtn") //fill in blank
+    const errorMsg: By = By.id("validHeader")// fill in blank 
 
-    test("filling in the blanks for real", () => {
-        await driver.findElement(hdrInput).sendKeys("Change this")
-        await driver.findElement(mkeInput).sendKeys("change this")
-        await driver.findElement(oaiInput).sendKeys("change this")
-        await driver.findElement(nameInput).sendKeys("change this")
-        await driver.findElement(submitBtn).click()
-        expect(errorMsg).toContain("Errors Received:")
+    test("filling in the blanks for real", async () => {
+        await driver.findElement(hdrInput).sendKeys("One long sentence")
+        await driver.findElement(mkeInput).sendKeys("four")
+        await driver.findElement(oaiInput).sendKeys("four")
+        await driver.findElement(nameInput).sendKeys("x")
+        await driver.findElement(submitBtn).click()    
+        await driver.findElement(errorMsg).click()
         await driver.findElement(clrBtn).click()
-        
+        expect(errorMsg.toString).toContain("Errors Received:")
     })
 })
