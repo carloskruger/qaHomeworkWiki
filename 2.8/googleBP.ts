b2385649bc77a3c738f1cef7c3f0630973490289
Carlos Kruger <carloskr@yahoo.com>
2022-11-16 20:20:22 -0500

    2.8.2

--- 2.8/googleBP.ts
+++ 2.8/googleBP.ts
@@ -1,11 +1,11 @@
-import {By} from "selenium-webdriver"
-import {BasePage} from "./basePage"
+import {By} from 'selenium-webdriver'
+import {BasePage} from './basePage'
 
 export class Google extends BasePage {
-    searchBar: By = By.name("q");
+    searchBar: By = By.name('q')
     results: By = By.id('rso')
     constructor() {
-        super({url: "https://www.google.com"});
+        super({url: "https://www.google.com"})
     }
     async search(searchTerm: string) {
         return this.setInput(this.searchBar, `${searchTerm}\n`)
